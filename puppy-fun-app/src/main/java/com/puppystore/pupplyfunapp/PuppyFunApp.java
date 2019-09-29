5f0d73d48f9567b42b02bc5f4540bd18bc3a812f
abhinavtrivedi1 <officialronuabhinav@gmail.com>
2019-09-30 02:55:29 +0530

    all files

---
+++ puppy-fun-app/src/main/java/com/puppystore/pupplyfunapp/PuppyFunApp.java
@@ -0,0 +1,13 @@
+package com.puppystore.pupplyfunapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuppyFunApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PuppyFunApp.class);
+    }
+
+}
---
+++ puppy-fun-app/src/main/java/com/puppystore/pupplyfunapp/PuppyFunAppConfiguration.java
@@ -0,0 +1,4 @@
+package com.puppystore.pupplyfunapp;
+
+public class PuppyFunAppConfiguration {
+}
